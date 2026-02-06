8d7f4e2e79fb58f5ca7c49ae65b129ebcf99c735
Sophiedevops <163428008+Sophiedevops@users.noreply.github.com>
2026-02-06 20:11:47 +0200

    Update proxy URLs in update3.sh script

--- scripts/update3.sh
+++ scripts/update3.sh
@@ -18,10 +18,11 @@ FILTER_PROTOCOLS="shadowsocks,vless,hysteria2,trojan,vmess"
 SUBS_LIST="
 https://raw.githubusercontent.com/sakha1370/OpenRay/refs/heads/main/output/all_valid_proxies.txt
 https://raw.githubusercontent.com/amirkma/proxykma/refs/heads/main/mix.txt
+https://raw.githubusercontent.com/mahdibland/V2RayAggregator/refs/heads/master/sub/sub_merge.txt
 https://raw.githubusercontent.com/DukeMehdi/FreeList-V2ray-Configs/refs/heads/main/Configs/ShadowSocks-V2Ray-Configs-By-DukeMehdi.txt
 https://raw.githubusercontent.com/gongchandang49/TelegramV2rayCollector/refs/heads/main/sub/mix
 https://raw.githubusercontent.com/SoliSpirit/v2ray-configs/refs/heads/main/Protocols/ss.txt
-https://raw.githubusercontent.com/LonUp/NodeList/main/node.txt
+https://raw.githubusercontent.com/hamedcode/port-based-v2ray-configs/main/sub/ss.txt
 "
 
 prepare_temp() {
